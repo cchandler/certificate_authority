dd2ffd65903840f550792eb2d5b317c92eeba33e
Justin Cummins <justin@squareup.com>
2012-05-02 19:41:56 -0700

    Certificate class builder from OpenSSL cert.

--- lib/certificate_authority/certificate.rb
+++ lib/certificate_authority/certificate.rb
@@ -176,5 +176,22 @@ module CertificateAuthority
       config
     end
 
+    def self.from_openssl openssl_cert
+      unless openssl_cert.is_a? OpenSSL::X509::Certificate
+        raise "Can only construct from an OpenSSL::X509::Certificate"
+      end
+
+      certificate = Certificate.new
+      # Only subject, key_material, and body are used for signing
+      certificate.distinguished_name = DistinguishedName.from_openssl openssl_cert.subject
+      certificate.key_material.public_key = openssl_cert.public_key
+      certificate.openssl_body = openssl_cert
+      certificate.serial_number.number = openssl_cert.serial.to_i
+      certificate.not_before = openssl_cert.not_before
+      certificate.not_after = openssl_cert.not_after
+      # TODO extensions
+      certificate
+    end
+
   end
 end
