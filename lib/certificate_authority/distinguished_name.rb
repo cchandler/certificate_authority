29d296135162b7626ee108cf9c354f87b583cff0
Eric Monti <esmonti@gmail.com>
2012-09-14 09:25:11 -0700

    apply ordering rules since openssl wont do it for us

--- lib/certificate_authority/distinguished_name.rb
+++ lib/certificate_authority/distinguished_name.rb
@@ -27,12 +27,12 @@ module CertificateAuthority
 
       # NB: the capitalization in the strings counts
       name = OpenSSL::X509::Name.new
-      name.add_entry("CN", common_name)
-      name.add_entry("O", organization) unless organization.blank?
-      name.add_entry("OU", organizational_unit) unless organizational_unit.blank?
-      name.add_entry("ST", state) unless state.blank?
-      name.add_entry("L", locality) unless locality.blank?
       name.add_entry("C", country) unless country.blank?
+      name.add_entry("L", locality) unless locality.blank?
+      name.add_entry("ST", state) unless state.blank?
+      name.add_entry("OU", organizational_unit) unless organizational_unit.blank?
+      name.add_entry("O", organization) unless organization.blank?
+      name.add_entry("CN", common_name)
       name
     end
 
